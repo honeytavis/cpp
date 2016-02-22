c763f75b4608211df214c16dc8aa5c2cf6faf2ef
tavis <email>
2016-02-23 00:13:24 +0800

    0222

---
+++ 0222/bool.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	bool i = true; 
+	bool j = false; 
+	std::cout << "true = " << i << std::endl; 
+	std::cout << "false = " << j << std::endl; 
+
+	return 0; 
+}
