a30096c325f02e09bd5e3f2bb0513ff09332d657
tavis <email>
2016-03-13 21:30:00 +0800

    const_ref.cc

--- 0311/reference/const_ref.cc
+++ 0311/reference/const_ref.cc
@@ -3,17 +3,21 @@
 int main()
 {
 	int a = 10; 
+	// 非常量左值引用只能绑定到非常量左值
 	int& refa = a; 
-
+	
+	// 常量左值引用可以绑定到所有类型
 	const int b = 3; 
 	const int& refb = b; 
-
 	const int& refc = a; 
 
+	// 常量左值引用可以绑定到左值和右值
 	const int& refd = 100; 
 	
+	// 右值引用
 	int&& refe = 100; // C++11
 
+	// 右值引用不可以绑定到左值
 	//int&& reff = a; 
 
 	return 0; 
